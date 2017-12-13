77c14c3fe54bc0211b86016b560d5ed001379d97
Leonardo Vitale <vitale@geru.com.br>
2017-12-13 16:47:02 -0200

    Parsing Twitter Data

--- src/components/tweet/index.js
+++ src/components/tweet/index.js
@@ -1,11 +1,37 @@
 import React from 'react';
 
-const Tweet = ({created_at, text, profile_background_image_url, profile_image_url}) => {
+function parseTwit(str) {
+  str = str.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&~\?\/.=]+/g, function (s) {
+    return s.link(s);
+  });
+  str = str.replace(/[@]+[A-Za-z0-9_]+/g, function (s) {
+    var user_name = s.replace('@', '');
+    return s.link("http://twitter.com/" + user_name);
+  });
+  str = str.replace(/[#]+[A-Za-z0-9_]+/g, function (s) {
+    var hashtag = s.replace('#', '');
+    return s.link("http://search.twitter.com/search?q=" + hashtag);
+  });
+  return str;
+}
+
+const renderHtml = () => <div>teste</div>
+
+const TEMP = props => `
+    ${renderHtml(...props)}
+`;
+
+const Tweet = ({ created_at, text, profile_background_image_url, profile_image_url }) => {
+  console.log(parseTwit(text));
   return (
     <div>
-      <img src={profile_image_url} alt="teste"/>
+      <img src={profile_image_url} alt="teste" />
       <div>{created_at}</div>
-      <div>{text}</div>
+      <div>
+        <TEMP />
+        <div dangerouslySetInnerHTML={{
+          __html: parseTwit(text)
+        }} /></div>
     </div>
   );
 };
